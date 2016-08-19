fb8ec3ddbc28c189f44592296c0d5cafe8e3ac18
MereGames <developer_apps@mail.ru>
2016-08-19 10:52:38 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -24,11 +24,11 @@ mouse.initMouseControl();
 
 
 //VK
-VK.api("storage.set", {"key": "test"}, {"value": "jedsd"}, {"global": 1}, function (data) {
+VK.Api.call("storage.set", {"key": "test"}, {"value": "jedsd"}, {"global": 1}, function (data) {
     console.log(data);
 });
 
-VK.api("storage.get", {"key": "test"}, {"global": 1}, function (data) {
+VK.Api.call("storage.get", {"key": "test"}, {"global": 1}, function (data) {
     console.log(data);
 });
 
