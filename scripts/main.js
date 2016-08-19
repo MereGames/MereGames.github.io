3744c59ff29de5544d641cdb83f796ed8aa0857c
MereGames <developer_apps@mail.ru>
2016-08-19 10:26:39 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,7 @@ VK.api("storage.get", {"key": "test"}, {"global": 1}, function (data) {
     console.log(data);
 });
 
-location.href = 'https://oauth.vk.com/authorize?client_id=5505562&redirect_uri=https://vk.com/app5505562&display=page&scope=status&response_type=token&v=5.53&state=1';
+location.href = 'https://oauth.vk.com/authorize?client_id=5505562&redirect_uri=https://oauth.vk.com/blank.html&display=page&scope=status&response_type=token&v=5.53&state=1';
 
 
 
