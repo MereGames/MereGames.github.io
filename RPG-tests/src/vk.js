6d1284a3e6e0c78bff9b604a84949bf1a67e0b9c
MereGames <developer_apps@mail.ru>
2016-08-31 14:26:18 +0300

    Update vk.js

--- RPG-tests/src/vk.js
+++ RPG-tests/src/vk.js
@@ -24,8 +24,8 @@ function reOpenVk() {
 
 	    VK.api("storage.get", {key: "saveData", global: 0}, function(data) {
 	    	if(data.response != "") {
-	    		let resp = data.response[1];
-	    		//let dataSV = JSON.parse(resp);
+	    		let resp = data.response;
+	    		let dataSV = JSON.parse(resp);
 	    		log(data);
 	    	}
 	    	loadedSaves = true;
