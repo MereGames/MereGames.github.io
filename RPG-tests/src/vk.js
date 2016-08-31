8011e31a1418069252f77cf6c49b7f9e8f96bd75
MereGames <developer_apps@mail.ru>
2016-08-31 15:33:46 +0300

    Update vk.js

--- RPG-tests/src/vk.js
+++ RPG-tests/src/vk.js
@@ -33,7 +33,7 @@ function reOpenVk() {
 	    		log(savedData);
 	    		
 	    		gameData.nextScaneId = startId;
-	    		mainPlayer.health = savedData[0].health;
+	    		mainPlayer.health = savedData.health;
         gameData.nextScaneName = startLocat;
         gameData.nextWorld = startWorld;
         gameData.numMusik = 3;
