9ee4d8421f56eab3d0415f6a11ce7fb09149a48d
MereGames <developer_apps@mail.ru>
2016-10-28 12:31:37 +0300

    Update time_load.js

--- Big War I/js/time_load.js
+++ Big War I/js/time_load.js
@@ -6,6 +6,6 @@
 //Mini-time for loading game locations(for all)
 var timesLoad = [
     {name: "butCon", time: 1500},
-    {name: "befoLogo", time: 1000},
+    {name: "befoLogo", time: 5000},
     {name: "inLevel", time: 5000}
 ];
